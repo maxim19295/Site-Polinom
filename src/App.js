b514246b9150057014181108cae380d12d18b734
maxim19295 <51368067+maxim19295@users.noreply.github.com>
2019-12-13 01:05:01 +0200

    Adding component Workplace

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Workplace from './components/Workplace/Workplace';
 import './App.css';
 
 function App() {
   return (
     <>
       <Header />
+      <Workplace />
     </>
   );
 }
