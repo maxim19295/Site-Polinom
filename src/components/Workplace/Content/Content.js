3e843769442a23effee5a0ea039ab8634bfe7f45
maxim19295 <51368067+maxim19295@users.noreply.github.com>
2019-12-13 01:28:00 +0200

    add Content

---
+++ src/components/Workplace/Content/Content.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const Content = (props) =>{
+    return <>
+        {/*Choosing method or entering coordinates or building graphic*/}
+            </>;
+}
+export default Content;
