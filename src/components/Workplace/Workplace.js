0870b4735b872da512e7ba3cbfa9134ba05c3f82
maxim19295 <51368067+maxim19295@users.noreply.github.com>
2019-12-13 01:03:32 +0200

    creating component Workplace

---
+++ src/components/Workplace/Workplace.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import w from './Workplace.module.css';
+const Workplace = (props) => {
+    return <div className={w.place}>
+            {/*WP_Header*/
+            /*Content*/}
+            </div>;
+}
+export default Workplace;
