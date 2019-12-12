9715715ed237aa8b3580113c9017b7f6960a26cc
maxim19295 <51368067+maxim19295@users.noreply.github.com>
2019-12-13 00:42:15 +0200

    Creating own redux as store.js

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+let rerenderTree = () =>{
+        console.log('empty function');
+    };
+export let subscribe = (observer) =>{
+        rerenderTree=observer;
+    };
+export let state = {
+        methods: [{type: 'none', text: '(не выбрано)'},{type: 'newton', text: 'Ньютона'},{type: 'Lagrangh', text: 'Лагранжа'}],
+        method: '(не выбрано)'
+    };
