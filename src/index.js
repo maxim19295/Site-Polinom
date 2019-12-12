b5acb998a4020a5b52c17de35a2f987bc3cd79ef
maxim19295 <51368067+maxim19295@users.noreply.github.com>
2019-12-13 00:46:20 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {state} from './redux/store.js';
+import {subscribe} from './redux/store.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let rerenderTree=(state)=>{return ReactDOM.render(<App state={state}/>, document.getElementById('root'));}
+rerenderTree(state);
+subscribe(()=>{rerenderTree(state)});
