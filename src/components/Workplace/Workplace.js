72dd558538aed8ce18d51c4d47b5617a79b491c5
maxim19295 <51368067+maxim19295@users.noreply.github.com>
2019-12-13 01:30:33 +0200

    Update Workplace.js
    Adding to Workplace components WP_Header and Content

--- src/components/Workplace/Workplace.js
+++ src/components/Workplace/Workplace.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import WP_Header from './WP_Header/WP_Header';
+import Content from './Content/Content';
 import w from './Workplace.module.css';
 const Workplace = (props) => {
     return <div className={w.place}>
-            {/*WP_Header*/
-            /*Content*/}
+            <WP_Header type_header={props.type_header}/>
+            <Content />
             </div>;
 }
 export default Workplace;
