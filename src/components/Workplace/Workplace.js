be62970139e9adb00b6c135d4dc271fc4a7d71ed
maxim19295 <51368067+maxim19295@users.noreply.github.com>
2019-12-13 13:06:52 +0200

    Update Workplace.js
    including component Content

--- src/components/Workplace/Workplace.js
+++ src/components/Workplace/Workplace.js
@@ -5,7 +5,7 @@ import w from './Workplace.module.css';
 const Workplace = (props) => {
     return <div className={w.place}>
             <WP_Header type_header={props.type_header}/>
-            <Content />
+            <Content type={props.type} needState={props.needState}/>
             </div>;
 }
 export default Workplace;
