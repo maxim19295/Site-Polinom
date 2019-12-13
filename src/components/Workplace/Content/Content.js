0ef127e192c120aa234169cebaf4abba7f0631bf
maxim19295 <51368067+maxim19295@users.noreply.github.com>
2019-12-13 13:05:39 +0200

    Update Content.js
    add component Method to Content

--- src/components/Workplace/Content/Content.js
+++ src/components/Workplace/Content/Content.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import Method from './Method/Method';
 const Content = (props) =>{
     return <>
+        <Method needState={props.needState}/>
         {/*Choosing method or entering coordinates or building graphic*/}
             </>;
 }
