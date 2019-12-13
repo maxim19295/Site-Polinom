4878f91ead7c35b469a4994182e70ec24ada3fd7
maxim19295 <51368067+maxim19295@users.noreply.github.com>
2019-12-13 13:43:45 +0200

    Update Content.js
    add component Graphic to Content

--- src/components/Workplace/Content/Content.js
+++ src/components/Workplace/Content/Content.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Method from './Method/Method';
 import Coordinates from './Coordinates/Coordinates';
+import Graphic from './Graphic/Graphic';
 const Content = (props) =>{
     return <>
         <Method needState={props.needState}/>
         <Coordinates />
+        <Graphic />
         {/*Choosing method or entering coordinates or building graphic*/}
             </>;
 }
