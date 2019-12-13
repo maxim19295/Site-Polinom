46190375e5726c9aaa30a26341fad6397d213df0
maxim19295 <51368067+maxim19295@users.noreply.github.com>
2019-12-13 11:58:30 +0200

    Update App.js
    Different state of components in dependense at url address

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Workplace from './components/Workplace/Workplace';
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom';
 
-function App() {
+function App(props) {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Workplace />
-    </>
+      <Route exact path='/' render={()=><Workplace type_header='Step 1. Choosing method of approximation' type='method' needState={{method: props.state.method, methods: props.state.methods}}/>}/>
+        <Route exact path='/coords' render={()=><Workplace type_header='Step 2. Enter coordinates' type='coords'/>}/>
+      <Route exact path='/graphic' render={()=><Workplace type_header='Step 3. Graphic building' type='graphic'/>}/>
+    </BrowserRouter>
   );
 }
 
