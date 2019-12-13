478a173647af472579a12615510f607604611066
maxim19295 <51368067+maxim19295@users.noreply.github.com>
2019-12-13 16:28:15 +0200

    Update Point.js

--- src/components/Workplace/Content/Coordinates/Point/Point.js
+++ src/components/Workplace/Content/Coordinates/Point/Point.js
@@ -2,8 +2,18 @@ import React from 'react';
 import point from './Point.module.css';
 import c from './../Coordinates.module.css';
 const Point = (props) =>{
-    let classPoint;
+    let classPoint, valuePoint;
     let newPointCoordinate=React.createRef();
+    if(props.line==='X')
+        {classPoint=' pointX';
+         if(props.needState.newX!==0){valuePoint=props.needState.newX;}
+         else valuePoint='';
+        }
+    else if(props.line==='Y')
+        {classPoint=' pointY';
+        if(props.needState.newY!==0){valuePoint=props.needState.newY;}
+         else valuePoint='';
+        }
     let onChangeNewPoint=()=>{
     props.onEnterNewPoint(props.line,newPointCoordinate.current.value);
     if((props.needState.newX==='')||(props.needState.newY===''))
@@ -14,7 +24,7 @@ const Point = (props) =>{
         classPoint=' pointX';
     else if(props.line==='Y')
         classPoint=' pointY';
-    return <input ref={newPointCoordinate} id={'newField'+props.line} type='number' className={point.point+classPoint} onInput={onChangeNewPoint}/>;
+    return <input ref={newPointCoordinate} id={'newField'+props.line} type='number' className={point.point+classPoint} onInput={onChangeNewPoint} value={valuePoint}/>;
         
 }
 export default Point;
