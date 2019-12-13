87cf1c5f9652a6ae3c6d531682d6e85fba348a2c
maxim19295 <51368067+maxim19295@users.noreply.github.com>
2019-12-13 18:14:52 +0200

    using clearState
    using function reset state after click on header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import h from './Header.module.css';
-const Header = () =>{
-    return <div  className={h.header}>Онлайн приближение функций</div>;
+import {NavLink} from 'react-router-dom';
+const Header = (props) =>{
+    const clearState = () => {
+        props.clearState();
+    }
+    return <NavLink to='/'><div className={h.header} onClick={clearState}>Онлайн приближение функций</div></NavLink>;
 }
 export default Header;
