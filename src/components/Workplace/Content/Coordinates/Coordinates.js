c1b2396158538202073c759489d9ddb46858cd57
maxim19295 <51368067+maxim19295@users.noreply.github.com>
2019-12-13 16:39:02 +0200

    show saved points

--- src/components/Workplace/Content/Coordinates/Coordinates.js
+++ src/components/Workplace/Content/Coordinates/Coordinates.js
@@ -5,6 +5,7 @@ import Button from './../Button/Button';
 import Point from './Point/Point';
 import PointLabel from './PointLabel/PointLabel';
 const Coordinates =(props)=>{
+    let Points = props.needState.point.map(point=><><PointLabel line={point.x} /><PointLabel line={point.y} /></>);
     let disabled_value;
     if((props.needState.newX==='')||(props.needState.newY===''))
         disabled_value='disabled';
@@ -14,9 +15,12 @@ const Coordinates =(props)=>{
             <PointLabel line='X' />
             <PointLabel line='Y' />
             </div>
+            <div className={c.pointBlock}>
+                {Points}
+            </div>
             <div>
-            <Point line='X' needState={props.needState} onEnterNewPoint={props.needFunctions.onInputNewCoordinate}/>
-            <Point line='Y' needState={props.needState} onEnterNewPoint={props.needFunctions.onInputNewCoordinate} />
+            <Point id='newXfield' line='X' needState={props.needState} onEnterNewPoint={props.needFunctions.onInputNewCoordinate}/>
+            <Point id='newYfield' line='Y' needState={props.needState} onEnterNewPoint={props.needFunctions.onInputNewCoordinate} />
             </div>
             <Button id={c.add_point} text='Добавить точку' needClickFunction={props.needFunctions.saveCoordinates} disabled={disabled_value} />
             <br/>
