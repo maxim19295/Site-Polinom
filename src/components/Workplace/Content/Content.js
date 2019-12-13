d59cef98ebe14455a94a308e97281bcb246a3421
maxim19295 <51368067+maxim19295@users.noreply.github.com>
2019-12-13 13:31:04 +0200

    Update Content.js
    add Coordinates to Content

--- src/components/Workplace/Content/Content.js
+++ src/components/Workplace/Content/Content.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Method from './Method/Method';
+import Coordinates from './Coordinates/Coordinates';
 const Content = (props) =>{
     return <>
         <Method needState={props.needState}/>
+        <Coordinates />
         {/*Choosing method or entering coordinates or building graphic*/}
             </>;
 }
