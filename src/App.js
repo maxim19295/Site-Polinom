f296df1d5f6961b054675ee33b6cf3f718ed0b32
maxim19295 <51368067+maxim19295@users.noreply.github.com>
2023-09-07 14:05:39 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import Header from './components/Header/Header';
-import Workplace from './components/Workplace/Workplace';
-import './App.css';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from 'react-router-dom';
+import Header from "./components/Header/Header";
+import Workplace from "./components/Workplace/Workplace";
 
-function App(props) {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Route exact path='/' render={()=><Workplace type_header='Step 1. Choosing method of approximation' type='method'/>}/>
-        <Route exact path='/coords' render={()=><Workplace type_header='Step 2. Enter coordinates' type='coords'/>}/>
-      <Route exact path='/graphic' render={()=><Workplace type_header='Step 3. Graphic building' type='graphic'/>}/>
-    </BrowserRouter>
-  );
-}
-
-export default App;
+export const App = () => (
+    <Router>
+        <Header/>
+        <Routes>
+            <Route exact path='/' element={<Workplace type_header='Step 1. Choosing method of approximation' type='method'/>}/>
+            <Route exact path='/coords' element={<Workplace type_header='Step 2. Enter coordinates' type='coords'/>}/>
+            <Route exact path='/graphic' element={<Workplace type_header='Step 3. Graphic building' type='graphic'/>}/>
+        </Routes>
+    </Router>
+);
