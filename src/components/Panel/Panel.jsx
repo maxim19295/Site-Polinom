254c38672e2d2e9a1fd6dee06dd65e3cb099ff42
maxim19295 <maxim19295@gmail.com>
2023-09-08 00:42:30 +0300

    Refactoring 2

--- src/components/Panel/Panel.jsx
+++ src/components/Panel/Panel.jsx
@@ -7,7 +7,25 @@ import GraphicContainer from "../Workplace/Content/Graphic/GraphicContainer";
 export const Panel = (props) =>{
     const number_step=props.state.number_step;
     let type_header, Content;
-    switch (number_step){
+        Content=<div className={p.place}>
+            <WP_Header type_header='Step 1. Choosing method of approximation'/>
+            <MethodContainer changeStep={()=> {
+                props.changeStep(2)
+            }}/>
+            {
+                (number_step>=2 && <div className={p.place}>
+                    <WP_Header type_header='Step 2. Enter coordinates'/>
+                    <CoordinatesContainer changeStep={()=>{props.changeStep(3)}}/>
+                </div>)
+            }
+            {
+                (number_step>=3 && <div className={p.place}>
+                    <WP_Header type_header='Step 3. Graphic building'/>
+                    <GraphicContainer/>
+                </div>)
+            }
+        </div>;
+    /*switch (number_step){
         case 1:
             Content=<div className={p.place}>
                 <WP_Header type_header='Step 1. Choosing method of approximation'/>
@@ -28,7 +46,7 @@ export const Panel = (props) =>{
                 <GraphicContainer/>
             </div>;
         break;
-        default: {}    }
+        default: {}    }*/
     console.log(props)
     return <div>
         {Content}
